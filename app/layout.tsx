6cd0ebe48af3802fcc05a50caaa03d7ff7b5c973
Gailcapulong <gailcapulong77@gmail.com>
2026-04-24 23:27:08 +0800

    feat: add Clerk authentication middleware and update dependencies
    - Added `proxy.ts` for Clerk middleware to protect routes under `/studio`.
    - Updated `package.json` to include new dependencies: `@clerk/nextjs`, `@clerk/themes`, `class-variance-authority`, `clsx`, `lucide-react`, `motion`, `next-themes`, `radix-ui`, `shadcn`, `tailwind-merge`, and `tw-animate-css`.
    - Modified `tsconfig.json` to include path mapping for `@/*`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider, SignInButton, SignUpButton, Show, UserButton } from "@clerk/nextjs";
+import { clerkModalAppearance } from "@/lib/clerk-modal-apperance";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClerkProvider appearance={clerkModalAppearance}>{children}</ClerkProvider>
       </body>
     </html>
   );
